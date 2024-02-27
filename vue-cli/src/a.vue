c1d3b448c39ad447110181693ed2a1c2f2f9d5e3
jsh-0808 <jsh080000@rootlab.xyz>
2024-02-27 17:28:32 +0900

    cli / form

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>header</div>
+</template>
+
+<script>
+export default {
+    // javascript - 인스턴스 옵션
+    methods: {
+        addNum: function() {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
